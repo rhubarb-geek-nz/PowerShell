4efff8870fe219aa05b90175c39ea8595ec8340f
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2024-01-13 09:01:01 +0000

    Update MSI 7.4.0 to 7.4.1

--- win/package.ps1
+++ win/package.ps1
@@ -52,14 +52,14 @@ If(!(test-path -PathType container "$SRCDIR"))
 @'
 <?xml version="1.0" encoding="UTF-8"?>
 <Wix xmlns="http://schemas.microsoft.com/wix/2006/wi">
-  <Product Id="*" Name="PowerShell 7 ARM64" Language="1033" Version="7.4.0.0" Manufacturer="Microsoft Corporation" UpgradeCode="31AB5147-9A97-4452-8443-D9709F0516E1">
-    <Package InstallerVersion="500" Compressed="yes" InstallScope="perMachine" Platform="arm64" Description="PowerShell 7.4.0 ARM64" Comments="PowerShell 7.4.0 ARM64" />
+  <Product Id="*" Name="PowerShell 7 ARM64" Language="1033" Version="7.4.1.0" Manufacturer="Microsoft Corporation" UpgradeCode="31AB5147-9A97-4452-8443-D9709F0516E1">
+    <Package InstallerVersion="500" Compressed="yes" InstallScope="perMachine" Platform="arm64" Description="PowerShell 7.4.1 ARM64" Comments="PowerShell 7.4.1 ARM64" />
     <MediaTemplate EmbedCab="yes" />
     <Feature Id="ProductFeature" Title="setup" Level="1">
       <ComponentGroupRef Id="ProductComponents" />
     </Feature>
     <Upgrade Id="{31AB5147-9A97-4452-8443-D9709F0516E1}">
-      <UpgradeVersion Maximum="7.4.0.0" Property="OLDPRODUCTFOUND" OnlyDetect="no" IncludeMinimum="yes" IncludeMaximum="no" />
+      <UpgradeVersion Maximum="7.4.1.0" Property="OLDPRODUCTFOUND" OnlyDetect="no" IncludeMinimum="yes" IncludeMaximum="no" />
     </Upgrade>
     <InstallExecuteSequence>
       <RemoveExistingProducts After="InstallInitialize" />
@@ -80,7 +80,7 @@ If(!(test-path -PathType container "$SRCDIR"))
       <Component Id="ApplicationShortcut" Guid="{D4A0639B-7BDD-4912-9489-FB8D227D507C}">
         <Shortcut Id="ApplicationStartMenuShortcut"
                   Name="PowerShell 7 (arm64)"
-                  Description="PowerShell 7.4.0 for ARM64"
+                  Description="PowerShell 7.4.1 for ARM64"
                   Target="[#pwsh.exe]"
                   Arguments="-WorkingDirectory ~"
                   WorkingDirectory="INSTALLDIR"/>
